981797526868f41f91c57d0918a41b640c7d3156
Charles Rethman <rethman@me.com>
2016-05-30 01:35:35 +0200

    edits to the sql to process vci and asi images, rs buffered feature sets
    Former-commit-id: e8e079b141af0b73c76db9c9507b51d439b6302b

--- sql/buffer_rs.sql
+++ sql/buffer_rs.sql
@@ -1,9 +1,19 @@
 DROP TABLE IF EXISTS zaf.buffer_vci;
 
+DROP TABLE IF EXISTS zaf.buffer_asi;
+
 CREATE TABLE zaf.buffer_vci (
   gid SERIAL PRIMARY KEY,
   the_geom GEOMETRY(MULTIPOLYGON, 201100),
   vci VARCHAR(15)
+  )
+;
+
+
+CREATE TABLE zaf.buffer_asi (
+  gid SERIAL PRIMARY KEY,
+  the_geom GEOMETRY(MULTIPOLYGON, 201100),
+  asi VARCHAR(15)
   )
 ;
 
@@ -32,4 +42,31 @@ INSERT INTO zaf.buffer_vci (
         ) AS g
       GROUP BY
         g.vci
+;
+
+INSERT INTO zaf.buffer_asi (
+  the_geom,
+  asi
+  )
+    SELECT
+        ST_Multi((ST_Dump(ST_Union(ST_Buffer(g.the_geom, 1500)))).geom),
+        g.asi AS asi
+      FROM (
+        SELECT
+            ST_Multi(f.the_geom) AS the_geom,
+            f.asi
+          FROM (
+            SELECT
+                (ST_Dump(ST_Union(the_geom))).geom AS the_geom,
+                asi_desc AS asi
+              FROM
+                zaf.ag_stress_idx_16013
+              GROUP BY
+                asi_desc
+            ) AS f
+          WHERE
+            ST_Area(f.the_geom) > 11335000
+        ) AS g
+      GROUP BY
+        g.asi
 ;
