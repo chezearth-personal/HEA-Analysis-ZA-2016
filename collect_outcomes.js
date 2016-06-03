35e29fca116a16db13db69ac156ccc1c6d1061f4
Charles Rethman <rethman@me.com>
2016-06-03 17:07:11 +0200

    Updating JS routine that read spreadsheets to load into psql
    Former-commit-id: c0627e29c9b16114f639c64e9ecb6b83e6cda469

--- collect_outcomes.js
+++ collect_outcomes.js
@@ -29,27 +29,35 @@ ask("Password", /.+/, function(pword) {
       if(err) {
         return console.error('could not connect to postgres', err);
       }
+      client.query('SELECT NOW() AS "theTime"', function(err, result) {
+        if(err) {
+          return console.error('error running query', err);
+        }
+        console.log(result.rows[0].theTime);
+        //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+        client.end();
+      });
       //Variable for storing all the spreadsheets data
       var lzAbbrevs = [
-        {"name" : "za_fw", "sheets" : [0,1,2]},
-        {"name" : "za_up", "sheets" : [0,1,2,3]},
-        {"name" : "za1xx", "sheets" : [0,1,2,3]},
-        {"name" : "za2xx", "sheets" : [0,1,2,3]},
-        {"name" : "za3xx", "sheets" : [0,1,2,3]},
-        {"name" : "zacni", "sheets" : [0,1,2,3]},
-        {"name" : "zakhc", "sheets" : [0,1,2,3]},
-        {"name" : "zalof", "sheets" : [0,1,2,3]},
-        {"name" : "zaloi", "sheets" : [0,1,2,3]},
-        {"name" : "zalrc", "sheets" : [0,1,2,3]},
-        {"name" : "zammo", "sheets" : [1,2,3]},
-        {"name" : "zancc", "sheets" : [0,1,2,3]},
-        {"name" : "zanfl", "sheets" : [0,1,2,3]},
-        {"name" : "zanoc", "sheets" : [0,1,2,3]},
-        {"name" : "zaocc", "sheets" : [0,1,2,3]},
-        {"name" : "zaolo", "sheets" : [0,1,2,3]},
-        {"name" : "zasco", "sheets" : [0,1,2,3]},
-        {"name" : "zaslc", "sheets" : [0,1,2,3]},
-        {"name" : "zatgl", "sheets" : [0,1,2]},
+        {"name" : "za_fw", code: 59050, "sheets" : [0,1,2]},
+        {"name" : "za_up", code: 59800, "sheets" : [0,1,2,3]},
+        {"name" : "za1xx", code: 59100, "sheets" : [0,1,2,3]},
+        {"name" : "za2xx", code: 59200, "sheets" : [0,1,2,3]},
+        {"name" : "za3xx", code: 59300, "sheets" : [0,1,2,3]},
+        {"name" : "zacni", code: 59106, "sheets" : [0,1,2,3]},
+        {"name" : "zakhc", code: 59208, "sheets" : [0,1,2,3]},
+        {"name" : "zalof", code: 59301, "sheets" : [0,1,2,3]},
+        {"name" : "zaloi", code: 59302, "sheets" : [0,1,2,3]},
+        {"name" : "zalrc", code: 59206, "sheets" : [0,1,2,3]},
+        {"name" : "zammo", code: 59210, "sheets" : [1,2,3]},
+        {"name" : "zancc", code: 59304, "sheets" : [0,1,2,3]},
+        {"name" : "zanfl", code: 59207, "sheets" : [0,1,2,3]},
+        {"name" : "zanoc", code: 59202, "sheets" : [0,1,2,3]},
+        {"name" : "zaocc", code: 59209, "sheets" : [0,1,2,3]},
+        {"name" : "zaolo", code: 59107, "sheets" : [0,1,2,3]},
+        {"name" : "zasco", code: 59305, "sheets" : [0,1,2,3]},
+        {"name" : "zaslc", code: 59203, "sheets" : [0,1,2,3]},
+        {"name" : "zatgl", code: 59105, "sheets" : [0,1,2]},
       ];
 
       var lzAffected = {
@@ -69,35 +77,48 @@ ask("Password", /.+/, function(pword) {
       }
 
       var deficit = {
-        fpl : 'T30',
-        lbpl : 'T31',
-        ubpl : 'T32',
+        fpl : {
+          cell: 'T30',
+          num: 1
+        },
+        lbpl : {
+          cell : 'T31',
+          num : 2
+        },
+        ubpl : {
+          cell : 'T32',
+          num : 3
+        },
       //  resilience : 'T33',
-        food : 'M31'
+        food : {
+          cell : 'M31',
+          num : 5
+        }
       };
 
       var outcome = {};
-//      var sql = 'INSERT INTO zaf.tbl_ofa_results (ofa_year, ofa_month, threshold, lz_affected, ' +
-//          'wg_affected, deficit) VALUES (';
+      var d = new Date();
+      var sqlString = 'INSERT INTO zaf.tbl_ofa_results (ofa_year, ofa_month, threshold, lz_affected, ' +
+          'wg_affected, wg, deficit) VALUES \n'
 
       for (var i = 0; i < lzAbbrevs.length; i++) {
       //  console.log('../spreadsheets/' + lzFiles[i] + '_0.xlsx');
         for (var subLz in lzAffected) {
           for (var subWG in wgAffected) {
-            console.log(lzAbbrevs[i].name + lzAffected[subLz].ext + wgAffected[subWG])
+//            console.log(lzAbbrevs[i].name + lzAffected[subLz].ext + wgAffected[subWG])
             /* Get the workbook */
             var workbook = XLSX.readFile('./spreadsheets/' + lzAbbrevs[i].name +
                 lzAffected[subLz].ext + wgAffected[subWG] + '.xlsx');
             /* DO SOMETHING WITH workbook HERE */
             for (var j = 0; j < lzAbbrevs[i].sheets.length; j++) {
-              var first_sheet_name = workbook.SheetNames[lzAbbrevs[i].sheets[j]];
+              var sheet_name = workbook.SheetNames[lzAbbrevs[i].sheets[j]];
 
               /* Get worksheet */
-              var worksheet = workbook.Sheets[first_sheet_name];
+              var worksheet = workbook.Sheets[sheet_name];
               outcome = {};
               /* Find desired cell */
               for (var thres in deficit) {
-                var desired_cell = worksheet[deficit[thres]];
+                var desired_cell = worksheet[deficit[thres].cell];
                 /* Get the value */
                 var desired_value = desired_cell.v;
                   if (thres === 'food') {
@@ -107,23 +128,18 @@ ask("Password", /.+/, function(pword) {
                   };
 
                   console.log(
-                      thres + ' deficit of ' + lzAbbrevs[i].name + lzAffected[subLz].ext + wgAffected[subWG] +
-                      ', ' + first_sheet_name + ' wg, is ' + outcome[thres]);
-                  sql +=
+                      thres + ' deficit of ' + lzAbbrevs[i].name + lzAffected[subLz].ext +
+                      wgAffected[subWG] + ', ' + sheet_name + ' wg, is ' + outcome[thres]);
+                  sqlString += lzAbbrevs[i].name +  '/' + sheet_name + ': (' + d.getFullYear() + ', ' + (d.getMonth() + 1) + ', ' +
+                      deficit[thres].num + ', ' + subLz + ', ' + (lzAbbrevs[i].sheets[j] + 1) + ', ' +
+                      subWG + ', ' + outcome[thres] + '),\n';
               }
             }
-//            client.query(sql, function(err, result))
-            client.query('SELECT NOW() AS "theTime"', function(err, result) {
-              if(err) {
-                return console.error('error running query', err);
-              }
-              console.log(result.rows[0].theTime);
-              //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-              client.end();
-            });
           }
         }
       }
+      sqlString = sqlString.substring(0, sqlString.length - 2) + ';';
+      console.log(sqlString);
     });
   });
 
