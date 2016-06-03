4f85ddc7bf50b121f54866c77e7b71b35fb8c175
Charles Rethman <rethman@me.com>
2016-06-03 14:47:32 +0200

    Adding JS routine to read spreadsheets
    Former-commit-id: 2d084ec6f39d86b3aae9c1c3e4c2334c8885c860

--- collect_outcomes.js
+++ collect_outcomes.js
@@ -1,70 +1,130 @@
-if(typeof require !== 'undefined') XLSX = require('xlsx');
-var pg = require('pg');
-var lzAbbrevs = [
-  {"name" : "za_fw", "sheets" : [0,1,2]},
-  {"name" : "za_up", "sheets" : [0,1,2,3]},
-  {"name" : "za1xx", "sheets" : [0,1,2,3]},
-  {"name" : "za2xx", "sheets" : [0,1,2,3]},
-  {"name" : "za3xx", "sheets" : [0,1,2,3]},
-  {"name" : "zacni", "sheets" : [0,1,2,3]},
-  {"name" : "zakhc", "sheets" : [0,1,2,3]},
-  {"name" : "zalof", "sheets" : [0,1,2,3]},
-  {"name" : "zaloi", "sheets" : [0,1,2,3]},
-  {"name" : "zalrc", "sheets" : [0,1,2,3]},
-  {"name" : "zammo", "sheets" : [1,2,3]},
-  {"name" : "zancc", "sheets" : [0,1,2,3]},
-  {"name" : "zanfl", "sheets" : [0,1,2,3]},
-  {"name" : "zanoc", "sheets" : [0,1,2,3]},
-  {"name" : "zaocc", "sheets" : [0,1,2,3]},
-  {"name" : "zaolo", "sheets" : [0,1,2,3]},
-  {"name" : "zasco", "sheets" : [0,1,2,3]},
-  {"name" : "zaslc", "sheets" : [0,1,2,3]},
-  {"name" : "zatgl", "sheets" : [0,1,2]},
-];
-var lzAffected = {
-    normal : {
-      code : 0,
-      ext : "_0"
-    },
-    drought : {
-      code: 1,
-      ext : "_1"
-    }
-};
-var wgAffected = {
-  grants : "",
-  noGrants : "_nogrants"
+var XLSX = require('xlsx'), pg = require('pg');
+
+
+function ask(question, format, callback) {
+ var stdin = process.stdin, stdout = process.stdout;
+
+ stdin.resume();
+ stdout.write(question + ": ");
+
+ stdin.once('data', function(data) {
+   data = data.toString().trim();
+
+   if (format.test(data)) {
+     callback(data);
+   } else {
+     stdout.write("It should match: "+ format +"\n");
+     ask(question, format, callback);
+   }
+ });
 }
-for (var i = 0; i < lzAbbrevs.length; i++) {
-//  console.log('../spreadsheets/' + lzFiles[i] + '_0.xlsx');
-  for (var propLz in lzAffected) {
-    for (var propWg in wgAffected) {
-      console.log(lzAffected[propLz].ext + wgAffected[propWg])
-      var workbook = XLSX.readFile('./spreadsheets/' + lzAbbrevs[i].name + lzAffected[propLz].ext
-          + wgAffected[propWg] + '.xlsx');
-      /* DO SOMETHING WITH workbook HERE */
-      for (var j = 0; j < lzAbbrevs[i].sheets.length; j++) {
-        var first_sheet_name = workbook.SheetNames[lzAbbrevs[i].sheets[j]];
-        var deficit = 'M31';
-/*    var deficit[1] = 'T31';
-        var deficit[2] = 'T32';
-        var deficit[3] = 'T33';
-        var deficit[4] = 'M31'; */
 
-        /* Get worksheet */
-        var worksheet = workbook.Sheets[first_sheet_name];
 
-        /* Find desired cell */
-        var desired_cell = worksheet[deficit];
+ask("Password", /.+/, function(pword) {
+    console.log("Your password (!) is: ", pword);
+    var conString = 'postgres://Charles:' + pword + '@localhost:5432/albers_ea';
+//    process.exit();
+    var client = new pg.Client(conString);
+    client.connect(function(err) {
+      if(err) {
+        return console.error('could not connect to postgres', err);
+      }
+      //Variable for storing all the spreadsheets data
+      var lzAbbrevs = [
+        {"name" : "za_fw", "sheets" : [0,1,2]},
+        {"name" : "za_up", "sheets" : [0,1,2,3]},
+        {"name" : "za1xx", "sheets" : [0,1,2,3]},
+        {"name" : "za2xx", "sheets" : [0,1,2,3]},
+        {"name" : "za3xx", "sheets" : [0,1,2,3]},
+        {"name" : "zacni", "sheets" : [0,1,2,3]},
+        {"name" : "zakhc", "sheets" : [0,1,2,3]},
+        {"name" : "zalof", "sheets" : [0,1,2,3]},
+        {"name" : "zaloi", "sheets" : [0,1,2,3]},
+        {"name" : "zalrc", "sheets" : [0,1,2,3]},
+        {"name" : "zammo", "sheets" : [1,2,3]},
+        {"name" : "zancc", "sheets" : [0,1,2,3]},
+        {"name" : "zanfl", "sheets" : [0,1,2,3]},
+        {"name" : "zanoc", "sheets" : [0,1,2,3]},
+        {"name" : "zaocc", "sheets" : [0,1,2,3]},
+        {"name" : "zaolo", "sheets" : [0,1,2,3]},
+        {"name" : "zasco", "sheets" : [0,1,2,3]},
+        {"name" : "zaslc", "sheets" : [0,1,2,3]},
+        {"name" : "zatgl", "sheets" : [0,1,2]},
+      ];
 
-        /* Get the value */
-        var desired_value = desired_cell.v;
+      var lzAffected = {
+          normal : {
+            code : 0,
+            ext : "_0"
+          },
+          drought : {
+            code: 1,
+            ext : "_1"
+          }
+      };
 
-        var result = Math.round(desired_value * 100,0);
+      var wgAffected = {
+        grants : "",
+        noGrants : "_nogrants"
+      }
 
-        console.log(
-          'Deficit of ' + lzAbbrevs[i].name + lzAffected[propLz].ext + wgAffected[propWg] +
-          ', ' + first_sheet_name + ' wg, is ' + result + '%');
-    }
-  }
-}}
+      var deficit = {
+        fpl : 'T30',
+        lbpl : 'T31',
+        ubpl : 'T32',
+      //  resilience : 'T33',
+        food : 'M31'
+      };
+
+      var outcome = {};
+//      var sql = 'INSERT INTO zaf.tbl_ofa_results (ofa_year, ofa_month, threshold, lz_affected, ' +
+//          'wg_affected, deficit) VALUES (';
+
+      for (var i = 0; i < lzAbbrevs.length; i++) {
+      //  console.log('../spreadsheets/' + lzFiles[i] + '_0.xlsx');
+        for (var subLz in lzAffected) {
+          for (var subWG in wgAffected) {
+            console.log(lzAbbrevs[i].name + lzAffected[subLz].ext + wgAffected[subWG])
+            /* Get the workbook */
+            var workbook = XLSX.readFile('./spreadsheets/' + lzAbbrevs[i].name +
+                lzAffected[subLz].ext + wgAffected[subWG] + '.xlsx');
+            /* DO SOMETHING WITH workbook HERE */
+            for (var j = 0; j < lzAbbrevs[i].sheets.length; j++) {
+              var first_sheet_name = workbook.SheetNames[lzAbbrevs[i].sheets[j]];
+
+              /* Get worksheet */
+              var worksheet = workbook.Sheets[first_sheet_name];
+              outcome = {};
+              /* Find desired cell */
+              for (var thres in deficit) {
+                var desired_cell = worksheet[deficit[thres]];
+                /* Get the value */
+                var desired_value = desired_cell.v;
+                  if (thres === 'food') {
+                    outcome[thres] = Math.round(desired_value * 100, 0) + '%';
+                  } else {
+                    outcome[thres] = Math.round(desired_value, 0);
+                  };
+
+                  console.log(
+                      thres + ' deficit of ' + lzAbbrevs[i].name + lzAffected[subLz].ext + wgAffected[subWG] +
+                      ', ' + first_sheet_name + ' wg, is ' + outcome[thres]);
+                  sql +=
+              }
+            }
+//            client.query(sql, function(err, result))
+            client.query('SELECT NOW() AS "theTime"', function(err, result) {
+              if(err) {
+                return console.error('error running query', err);
+              }
+              console.log(result.rows[0].theTime);
+              //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+              client.end();
+            });
+          }
+        }
+      }
+    });
+  });
+
+//Connection string to Postgres
