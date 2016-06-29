23cb4b6c22d1d6b58321d046d6ebe0b5f6833f54
Charles Rethman <rethman@me.com>
2016-06-29 12:16:33 +0200

    Updating pg_dump shell script to create db dump files <49MB
    Former-commit-id: 66c1e493406f46f3b1d2539ace9ad311ff4a778a

--- dump_dbs.sh
+++ dump_dbs.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-pg_dump -COF c -n zaf -T zaf.demog_eas -T 'zaf.frame*' -T zaf.grid -T 'zaf.hydro*' -T 'zaf.infra*' -T 'zaf.landcap*'  -T 'zaf.landcover*' -T zaf.landtenure_farms -T zaf.livezones_rural_old -T zaf.sas_affected_2015 -T zaf.tbl_assessments -T 'zaf.tbl_ind*' -T 'zaf.tbl_nisis*' -T zaf.tbl_sampled_villages -T 'zaf.tmp*' albers_ea | split -b 98m - db/zaf.dump
+pg_dump -COF c -n zaf -T zaf.demog_eas -T 'zaf.frame*' -T zaf.grid -T 'zaf.hydro*' -T 'zaf.infra*' -T 'zaf.landcap*'  -T 'zaf.landcover*' -T zaf.landtenure_farms -T zaf.livezones_rural_old -T zaf.sas_affected_2015 -T zaf.tbl_assessments -T 'zaf.tbl_ind*' -T 'zaf.tbl_nisis*' -T zaf.tbl_sampled_villages -T 'zaf.tmp*' albers_ea | split -b 49m - db/zaf_dump_
