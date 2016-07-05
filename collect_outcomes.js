c39ba180e3105711c281add06fce9c3117ffac74
Charles Rethman <rethman@me.com>
2016-07-05 08:55:31 +0200

    Updating collect_outcomes.js by reodering the functions
    Former-commit-id: 19318ce32f26c39164d19385e734d0f9a4ffc947

--- collect_outcomes.js
+++ collect_outcomes.js
@@ -2,6 +2,54 @@
 var XLSX = require('xlsx'), pg = require('pg');
 const fs = require('fs');
 
+
+/*
+ * Get any other user input such as user name, required dates or yes/no options. Data format can
+ * also be defined in the 'format' parameter (using a RegExp).
+ *
+ * @param question {String} Optional. Question to be asked on StdIn. If it ends in '?', ':' or ')'
+ * it will be append with a space only; if it ends with anything else it will be appended with '? '.
+ * Default 'Enter: '.
+ * @param format {RegExp}. Displays format as a regexp to the user. Default `/\w+|\s+/`, any number
+ * of alphanumeric or whitespace characters allowed, nothing not allowed (  ).
+ * @param callback {Function}. `Function (cancelled, data)` where cancelled is true if user aborts
+ * (Ctrl-C).
+ *
+ */
+function ask(question, format, callback) {
+   var stdin = process.stdin, stdout = process.stdout;
+   if (question === undefined) stdout.write("Enter: ");
+   else if (question.trim().slice(-1) == ':' || question.trim().slice(-1) == '?' || question.trim().slice(-1) == ')') stdout.write(question.trim() + ' ');
+   else stdout.write(question + '? ');
+   if (format === undefined) format = /\w+|\s+/;
+   stdin.setEncoding('utf8');
+
+   stdin.resume();
+   stdin.once('data', function(data) {
+      if (data.length > 1) {
+         data = data.toString().trim();
+      } else {
+         data = data.toString();
+      }
+      if (data == '\u0003') {
+         callback(true);
+         stdout.write('\n'); // add a line and quit
+         process.exit();
+      }
+      if (format.test(data)) {
+         // clear any extraneous single characters in StdOut
+         stdout.write('\u0008');
+         stdout.write('\u007F');
+         stdout.write('\u0008');
+         callback(false, data);
+      } else {
+         stdout.write("It should match: "+ format +"\n");
+         ask(question, format, callback);
+      }
+   });
+}
+
+
 /*
  * Get a password from stdin.
  * Adapted from <http://stackoverflow.com/a/10357818/122384>.
@@ -11,10 +59,9 @@ const fs = require('fs');
  *      `cancelled` is true if the user aborted (Ctrl+C).
  * [CR]Added in nice fat bullet placeholders ('\u2022').
  * [CR]Fixed the backspace to trim off last placeholders and snip password string at end.
- * [CR]Fixed Ctrl-C to exit process (Quit).
+ * [CR]Fixed Ctrl-C (Quit) to add a line before the exit.
  *
  */
-
 function getPassword(prompt, callback) {
    var stdin = process.stdin, stdout = process.stdout
    if (callback === undefined) {
@@ -71,205 +118,6 @@ function getPassword(prompt, callback) {
 }
 
 
-
-/*
- * Get any other user input such as user name, required dates or yes/no options. Data format can
- * also be defined in the 'format' parameter (using a RegExp).
- *
- * @param question {String} Optional. Question to be asked on StdIn. If it ends in '?', ':' or ')'
- * it will be append with a space only; if it ends with anything else it will be appended with '? '.
- * Default 'Enter: '.
- * @param format {RegExp}. Displays format as a regexp to the user. Default `/\w+|\s+/`, any number
- * of alphanumeric or whitespace characters allowed, nothing not allowed (  ).
- * @param callback {Function}. `Function (cancelled, data)` where cancelled is true if user aborts
- * (Ctrl-C).
- *
- */
-
-function ask(question, format, callback) {
-   var stdin = process.stdin, stdout = process.stdout;
-   if (question === undefined) stdout.write("Enter: ");
-   else if (question.trim().slice(-1) == ':' || question.trim().slice(-1) == '?' || question.trim().slice(-1) == ')') stdout.write(question.trim() + ' ');
-   else stdout.write(question + '? ');
-   if (format === undefined) format = /\w+|\s+/;
-   stdin.setEncoding('utf8');
-
-   stdin.resume();
-   stdin.once('data', function(data) {
-      if (data.length > 1) {
-         data = data.toString().trim();
-      } else {
-         data = data.toString();
-      }
-      if (data == '\u0003') {
-         callback(true);
-         stdout.write('\n'); // add a line and quit
-         process.exit();
-      }
-      if (format.test(data)) {
-         // clear any extraneous single characters in StdOut
-         stdout.write('\u0008');
-         stdout.write('\u007F');
-         stdout.write('\u0008');
-         callback(false, data);
-      } else {
-         stdout.write("It should match: "+ format +"\n");
-         ask(question, format, callback);
-      }
-   });
-}
-
-
-/*
- * Function to read all the spreadsheet values and load them into an object, as well as creating an
- * SQL INSERT values query string for loading the spreadsheet outputs into a Postgres table.
- *
- * @param sqlString {string} Required. First part of SQL string to which the results from the
- * spreadsheet reads are appended in correct syntax and returned.
- * @param ofa {Array} Required. Two elements with numbers representing month ofa[0] ('1' = January)
- * and year ofa[1].
- *
- */
-
-function readSpreadSheets(sqlString, lzAbbrevs, deficits, ofa) {
-   console.log('Reading spreadsheets...');
-         // Object with the LZ affectedness groupings.
-         var lzAffected = {
-            normal : {
-               code : 0,
-               ext : "_0"
-            },
-            drought : {
-               code: 1,
-               ext : "_1"
-            }
-         };
-      // Object with the wealth group affectedness groupings.
-      var wgAffected = {
-         grants : "",
-         noGrants : "_nogrants"
-      }
-
-      // Variable for storing outcomes
-      var outcome = {};
-      //'name' is the LZ abbrev name in the spreadsheet file name, 'code' is the LZ code (for the DB table), 'wgs' array contains objects with worksheet numbers (in the spreadsheet) and WG IDs from tbl_wgs in each LZ analysis.
-      for (var i = 0; i < lzAbbrevs.length; i++) {
-         for (var subLz in lzAffected) {
-            for (var subWG in wgAffected) {
-               // Get the workbook
-               var workbook = XLSX.readFile('./spreadsheets/' + lzAbbrevs[i].name + lzAffected[subLz].ext + wgAffected[subWG] + '.xlsx');
-               process.stdout.write('./spreadsheets/' + lzAbbrevs[i].name + lzAffected[subLz].ext +
-               wgAffected[subWG] + '.xlsx\n');
-               // Get the worksheet and assign it to a variable
-               for (var j = 0; j < lzAbbrevs[i].wgs.length; j++) {
-                  var sheet_name = workbook.SheetNames[lzAbbrevs[i].wgs[j].sheet];
-                  var worksheet = workbook.Sheets[sheet_name];
-                  //reset the outcome object
-                  outcome = {};
-                  // Find desired cell
-                  for (var thres in deficits) {
-                     var desired_cell = worksheet[deficits[thres].cell];
-                     // Get the value
-                     var desired_value = desired_cell.v;
-                     if (thres === 'food') {
-                        outcome[thres] = Math.round(desired_value * 100, 0) + '%';
-                     } else {
-                        outcome[thres] = Math.round(desired_value, 0);
-                     };
-
-                     sqlString += '(' + ofa[1] + ', ' + ofa[0] + ', ' + lzAbbrevs[i].code + ', ' + (lzAbbrevs[i].wgs[j].wg) + ', \u0027' + subLz + '\u0027, \u0027' + subWG + '\u0027, \u0027' + deficits[thres].descr + '\u0027, ' + desired_value + '),\n';
-                  }
-               }
-            }
-         }
-      }
-      // Query SQL string for inserting data into zaf.tbl_ofa_analysis postgres table
-      sqlString = sqlString.substring(0, sqlString.length - 2) + '\n;';
-      return sqlString
-//   });
-}
-
-
-
-/*
- * Get the database time. Simple routine to throw out the time on the Database when the update
- * (INSERT or DELETE) is finished.
- *
- * @param pgClient {Object}. Required. Postgres client connection object must be passed.
- *
- */
-
-function getDbTime(pgClient) {
-  // Query to get a time stamp from the DB(!) for the succesful completion of the work
-  pgClient.query('SELECT NOW() AS "theTime"', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    // Success. Output is something like Tue Jun 21 2016 10:12:47 GMT+0200 (SAST)
-    console.log(result.rows[0].theTime);
-    // end client session*/
-    pgClient.end();
-    process.exit();
-    });
-
-}
-
-/*
- * Deletes previous record for the same analysis date and uploads the current analysis results
- * collected from the spreadsheets.
- *
- * @param pgClient {Object}. Required. Postgres client connection object must be passed.
- * @param ofa {Array} Required. Two elements with numbers representing month ofa[0] ('1' = January)
- * and year ofa[1].
- * @param deleteOnly {Boolean}. Optional. TRUE when data for analysis are deleted but not reinserted.
- * Default FALSE.
- *
- */
-
-function loadTable(pgClient, ofa, deleteOnly) {
-   // Query to first delete existing data in zaf.tbl_ofa_analysis for the desired month and year
-   pgClient.query('DELETE FROM zaf.tbl_ofa_analysis WHERE ofa_year = ' + ofa[1] + ' AND ofa_month = ' + ofa[0] + ';', function(err, result) {
-      if(err) {
-         return console.error('error running DELETE query', err);
-      }
-      // Success. Output is something like DELETE: 1168 rows affected
-      console.log(result.command + ': ' + result.rowCount + ' rows affected');
-      // Query to insert the new data using the SQL string above
-      if (!deleteOnly) {
-         // Create the INSERT SQL String
-         var sqlString = 'INSERT INTO zaf.tbl_ofa_analysis (ofa_year, ofa_month, lz_code, wg_code, ' + 'lz_affected, wg_affected, threshold, deficit) VALUES \n';
-         // Read the config files containing info on analysis spreadsheets and deficits.
-         // Read the config file for the spreadsheet structure
-         fs.readFile("./config_spreadsheets.json", function(err, sSheetData) {
-            if (err) {
-                  console.log("LZ and spreadsheet config file missing or corrupt.");
-                  return;
-            }
-            // Success. Read the config file for thresholds
-            fs.readFile("./config_deficits.json", function(err, deficitsData) {
-               if (err) {
-                  console.log("Deficits config file missing or corrupt.");
-                  return
-               }
-               // Success. Parse the files and pass (sic!) them on to the readSpreadSheets function
-               sqlString = readSpreadSheets(sqlString, JSON.parse(sSheetData.toString()), JSON.parse(deficitsData.toString()), ofa);
-               pgClient.query(sqlString, function(err, result) {
-                  if(err) {
-                     return console.error('error running INSERT query', err);
-                  }
-                  // Success. Output is something like INSERT: 1168 rows affected
-                  console.log(result.command + ': ' + result.rowCount + ' rows affected');
-                  getDbTime(pgClient);
-               });
-            });
-         });
-      } else {
-         getDbTime(pgClient);
-      }
-   });
-}
-
-
 /*
  * Connects to the DB and selects which analysis (month, year) the user wants load into it.
  *
@@ -277,7 +125,6 @@ function loadTable(pgClient, ofa, deleteOnly) {
  * it must be passed.
  *
  */
-
 function connectDB(pgClient) {
    // Connect the client to the database
    pgClient.connect(function(err) {
@@ -347,6 +194,151 @@ function connectDB(pgClient) {
 }
 
 
+/*
+ * Deletes previous record for the same analysis date and uploads the current analysis results
+ * collected from the spreadsheets.
+ *
+ * @param pgClient {Object}. Required. Postgres client connection object must be passed.
+ * @param ofa {Array} Required. Two elements with numbers representing month ofa[0] ('1' = January)
+ * and year ofa[1].
+ * @param deleteOnly {Boolean}. Optional. TRUE when data for analysis are deleted but not reinserted.
+ * Default FALSE.
+ *
+ */
+function loadTable(pgClient, ofa, deleteOnly) {
+   // Query to first delete existing data in zaf.tbl_ofa_analysis for the desired month and year
+   pgClient.query('DELETE FROM zaf.tbl_ofa_analysis WHERE ofa_year = ' + ofa[1] + ' AND ofa_month = ' + ofa[0] + ';', function(err, result) {
+      if(err) {
+         return console.error('error running DELETE query', err);
+      }
+      // Success. Output is something like DELETE: 1168 rows affected
+      console.log(result.command + ': ' + result.rowCount + ' rows affected');
+      // Query to insert the new data using the SQL string above
+      if (!deleteOnly) {
+         // Create the INSERT SQL String
+         var sqlString = 'INSERT INTO zaf.tbl_ofa_analysis (ofa_year, ofa_month, lz_code, wg_code, ' + 'lz_affected, wg_affected, threshold, deficit) VALUES \n';
+         // Read the config files containing info on analysis spreadsheets and deficits.
+         // Read the config file for the spreadsheet structure
+         fs.readFile("./config_spreadsheets.json", function(err, sSheetData) {
+            if (err) {
+                  console.log("LZ and spreadsheet config file missing or corrupt.");
+                  return;
+            }
+            // Success. Read the config file for thresholds
+            fs.readFile("./config_deficits.json", function(err, deficitsData) {
+               if (err) {
+                  console.log("Deficits config file missing or corrupt.");
+                  return
+               }
+               // Success. Parse the files and pass (sic!) them on to the readSpreadSheets function
+               sqlString = readSpreadSheets(sqlString, JSON.parse(sSheetData.toString()), JSON.parse(deficitsData.toString()), ofa);
+               pgClient.query(sqlString, function(err, result) {
+                  if(err) {
+                     return console.error('error running INSERT query', err);
+                  }
+                  // Success. Output is something like INSERT: 1168 rows affected
+                  console.log(result.command + ': ' + result.rowCount + ' rows affected');
+                  getDbTime(pgClient);
+               });
+            });
+         });
+      } else {
+         getDbTime(pgClient);
+      }
+   });
+}
+
+
+/*
+ * Read all the spreadsheet values and load them into an object, as well as creating an SQL INSERT
+ * values query string for loading the spreadsheet outputs into a Postgres table.
+ *
+ * @param sqlString {string} Required. First part of SQL string to which the results from the
+ * spreadsheet reads are appended in correct syntax and returned.
+ * @param ofa {Array} Required. Two elements with numbers representing month ofa[0] ('1' = January)
+ * and year ofa[1].
+ *
+ */
+function readSpreadSheets(sqlString, lzAbbrevs, deficits, ofa) {
+   console.log('Reading spreadsheets...');
+         // Object with the LZ affectedness groupings.
+         var lzAffected = {
+            normal : {
+               code : 0,
+               ext : "_0"
+            },
+            drought : {
+               code: 1,
+               ext : "_1"
+            }
+         };
+      // Object with the wealth group affectedness groupings.
+      var wgAffected = {
+         grants : "",
+         noGrants : "_nogrants"
+      }
+
+      // Variable for storing outcomes
+      var outcome = {};
+      //'name' is the LZ abbrev name in the spreadsheet file name, 'code' is the LZ code (for the DB table), 'wgs' array contains objects with worksheet numbers (in the spreadsheet) and WG IDs from tbl_wgs in each LZ analysis.
+      for (var i = 0; i < lzAbbrevs.length; i++) {
+         for (var subLz in lzAffected) {
+            for (var subWG in wgAffected) {
+               // Get the workbook
+               var workbook = XLSX.readFile('./spreadsheets/' + lzAbbrevs[i].name + lzAffected[subLz].ext + wgAffected[subWG] + '.xlsx');
+               process.stdout.write('./spreadsheets/' + lzAbbrevs[i].name + lzAffected[subLz].ext +
+               wgAffected[subWG] + '.xlsx\n');
+               // Get the worksheet and assign it to a variable
+               for (var j = 0; j < lzAbbrevs[i].wgs.length; j++) {
+                  var sheet_name = workbook.SheetNames[lzAbbrevs[i].wgs[j].sheet];
+                  var worksheet = workbook.Sheets[sheet_name];
+                  //reset the outcome object
+                  outcome = {};
+                  // Find desired cell
+                  for (var thres in deficits) {
+                     var desired_cell = worksheet[deficits[thres].cell];
+                     // Get the value
+                     var desired_value = desired_cell.v;
+                     if (thres === 'food') {
+                        outcome[thres] = Math.round(desired_value * 100, 0) + '%';
+                     } else {
+                        outcome[thres] = Math.round(desired_value, 0);
+                     };
+
+                     sqlString += '(' + ofa[1] + ', ' + ofa[0] + ', ' + lzAbbrevs[i].code + ', ' + (lzAbbrevs[i].wgs[j].wg) + ', \u0027' + subLz + '\u0027, \u0027' + subWG + '\u0027, \u0027' + deficits[thres].descr + '\u0027, ' + desired_value + '),\n';
+                  }
+               }
+            }
+         }
+      }
+      // Query SQL string for inserting data into zaf.tbl_ofa_analysis postgres table
+      sqlString = sqlString.substring(0, sqlString.length - 2) + '\n;';
+      return sqlString
+}
+
+
+/*
+ * Get the database time. Simple routine to throw out the time on the Database when the update
+ * (INSERT or DELETE) is finished.
+ *
+ * @param pgClient {Object}. Required. Postgres client connection object must be passed.
+ *
+ */
+function getDbTime(pgClient) {
+  // Query to get a time stamp from the DB(!) for the succesful completion of the work
+  pgClient.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    // Success. Output is something like Tue Jun 21 2016 10:12:47 GMT+0200 (SAST)
+    console.log(result.rows[0].theTime);
+    // end client session*/
+    pgClient.end();
+    process.exit();
+    });
+
+}
+
 
 /*
  * Callers for getting user inputs for connecting to the database. Upon entry of credentials, the
@@ -354,8 +346,7 @@ function connectDB(pgClient) {
  * authentication/authorisation at this stage.
  *
  */
-
-// Get the user name
+// Get the DB user name
 ask('\nYou may need account credentials to connect to Postgres. However, if you\ndownloaded the database dump file from GitHub, you may ignore the user name\nand password below (skip by pressing ENTER twice)\n\nPostgres user name', /.+|\s/, function(cancel, user_name) {
    if (!cancel) {
       // Get the DB password
